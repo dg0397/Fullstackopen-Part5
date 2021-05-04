574dd4b3f0c0c07e8890328aa266fdf26554117b
dg0397 <rgg0397@gmail.com>
2021-05-03 23:51:57 -0400

    5.14*: Blog list tests, step2

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render,fireEvent } from '@testing-library/react'
 //import { prettyDOM } from '@testing-library/dom'
 import Blog from './Blog'
 
@@ -36,7 +36,7 @@ describe('renders content by default',() => {
     expect(component.container).toHaveTextContent(blog.author)
   })
 
-  test('url and likes not renders by default',() => {
+  test('url and likes are not renders by default',() => {
     //check if url is not visible
     expect(component.container).not.toHaveTextContent(blog.url)
     //check if number of likes is not visible
@@ -56,4 +56,43 @@ describe('renders content by default',() => {
   //const blogDetailsToEnd = component.container.querySelector('.blogDetails')
   //expect(blogDetailsToEnd).toBeDefined
   //component.debug()
+})
+
+describe('clicking the button, blog details is rendered',() => {
+  let component
+
+  const blog = {
+    'author': 'dg0397',
+    'likes': 35,
+    'title': 'Learning GraphQL',
+    'url': 'leargrapgql.com',
+    'user': {
+      'username': 'dg0397',
+      'name': 'Dionisio',
+      'id': '6058e0392a3fc348a4bf8586'
+    },
+    'id': '605b74bcf0e9ae4a709879db'
+  }
+
+  beforeEach(() => {
+    component = render(
+      <Blog blog = {blog}  user = {blog.user}  />
+    )
+
+    const button = component.getByText('view')
+    fireEvent.click(button)
+  })
+
+  test('blogDetails component is rendered',() => {
+    const div = component.container.querySelector('.blogDetails')
+    expect(div).toBeDefined()
+  })
+  test('url and likes are rendered',() => {
+    //check if url is visible
+    const urlElement = component.getByText(blog.url)
+    expect(urlElement).toBeDefined()
+    //check if number of likes is visible
+    const authorElement = component.getByText(`likes ${blog.likes}`)
+    expect(authorElement).toBeDefined()
+  })
 })
